d86b2f547a75c116a868595c49e68092b95bce4a
NakHyun Choi <9naknak8@gmail.com>
2023-01-22 17:42:56 +0900

    FAQ_Recyclerview
    Get data from Firebase and show it on the screen throw recyclerview.
    And if user clicks the recyclerview, program get FAQ from firebase and sprinkle data to the screen

--- YangDo_Platform/app/src/main/java/se/jbnu/yangdoplatform/Board/Board_From_FAQ_Activity.kt
+++ YangDo_Platform/app/src/main/java/se/jbnu/yangdoplatform/Board/Board_From_FAQ_Activity.kt
@@ -2,39 +2,55 @@ package se.jbnu.yangdoplatform.Board
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import se.jbnu.yangdoplatform.R
+import se.jbnu.yangdoplatform.model.BoardModel
+import se.jbnu.yangdoplatform.model.ChatModel
 
 class Board_From_FAQ_Activity : AppCompatActivity() {
-    var title_tv: TextView? = null
-    var content_tv: TextView? = null
-    var date_tv: TextView? = null
-    var comment_layout: LinearLayout? = null
-    var comment_et: EditText? = null
-    var reg_button: Button? = null
+
     private var clickedFQATitle: String? = null
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_board_detail)
-        // 컴포넌트 초기화
-        title_tv = findViewById(R.id.title_tv)
-        content_tv = findViewById(R.id.content_tv)
-        date_tv = findViewById(R.id.date_tv)
-        comment_layout = findViewById(R.id.comment_layout)
-        comment_et = findViewById(R.id.comment_et)
-        reg_button = findViewById(R.id.reg_button)
+        setContentView(R.layout.activity_fqa_detail_board)
 
+        var fqa_detail_board_title: TextView = findViewById(R.id.fqa_detail_board_title)
+        var fqa_detail_board_content: TextView = findViewById(R.id.fqa_detail_board_content)
+
+        val fqa_board_back_button : ImageButton = findViewById(R.id.fqa_detail_board_back_button)
+        fqa_board_back_button.setOnClickListener(View.OnClickListener {
+            onBackPressed()
+        })
+
+        //클릭으로 받아온 title
         clickedFQATitle = intent.getStringExtra("clickedFQATitle")
 
+        FirebaseDatabase.getInstance().reference.child("FAQ").addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (item in dataSnapshot.children) {
+                    var title: String = item.getValue(BoardModel::class.java)?.title.toString()
+                    if(title.equals(clickedFQATitle)){
+                        fqa_detail_board_content.setText(item.getValue(BoardModel::class.java)?.content)
+                    }
+                }
+            }
 
-        // 등록하기 버튼을 눌렀을 때 댓글 등록 함수 호출
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+
+
+
+        fqa_detail_board_title.setText(clickedFQATitle)
+
+
 
     }
 }
