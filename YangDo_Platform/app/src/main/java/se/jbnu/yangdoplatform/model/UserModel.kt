d2b375fcaf63b310b26314f7a85afc3a1012aa07
NakHyun Choi <9naknak8@gmail.com>
2023-01-31 17:28:40 +0900

    forSaving

--- YangDo_Platform/app/src/main/java/se/jbnu/yangdoplatform/model/UserModel.kt
+++ YangDo_Platform/app/src/main/java/se/jbnu/yangdoplatform/model/UserModel.kt
@@ -1,9 +1,7 @@
 package se.jbnu.yangdoplatform.model
 
 class UserModel {
-    @JvmField
     var userName: String? = null
-    @JvmField
     var uid: String? = null
     var profileImageUrl: String? = null
 }
